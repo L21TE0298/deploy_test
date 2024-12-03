b038bc2b4d451b83aff9a90ac45194b360d83000
L21TE0298 <l21te0298@teziutlan.tecnm.mx>
2024-12-03 10:53:03 -0600

    UPDATED: Deploy redirect

--- src/main/java/com/ghostappi/backend/config/SecurityConfig.java
+++ src/main/java/com/ghostappi/backend/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/nutrients/**","/rewards/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("https://deploy-test-1-wylo.onrender.com/doc/swagger.html", true)
+                        .defaultSuccessUrl("https://deploy-test-1-wylo.onrender.com/doc/swagger-ui/index.html#/", true)
                         .failureUrl("/login?error=true"))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
